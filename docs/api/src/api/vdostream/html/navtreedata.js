ebbc720a22313488b46686af8e2d027b74f7fbe8
AxisCommunications - Ecosystem service account <144934332+github-axiscommunications-ecosystem@users.noreply.github.com>
2023-11-20 10:04:41 +0100

    Update API VERSION to 4.11 (#240)
    Co-authored-by: github-axiscommunications-ecosystem <github-axiscommunications-ecosystem@users.noreply.github.com>

--- docs/api/src/api/vdostream/html/navtreedata.js
+++ docs/api/src/api/vdostream/html/navtreedata.js
@@ -25,7 +25,7 @@ var NAVTREE =
 var NAVTREEINDEX =
 [
 "../../../main/html/acap3_api.html",
-"vdo-types_8h.html#acaa9291dd03e128aa2097c75b0d65ab8ac3db05fc3647260a8408dc0a00636b49"
+"vdo-types_8h.html#acaa9291dd03e128aa2097c75b0d65ab8a9668eddb4acce5e6fd23ee924461d2aa"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
